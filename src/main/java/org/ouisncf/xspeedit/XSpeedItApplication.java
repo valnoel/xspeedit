f4ac525c9bc3451e1b535f7c3beebef2b236cfc6
Valentin NOEL <valent.noel@gmail.com>
2018-09-11 11:16:21 +0200

    Add new XSpeedItApplication for simple packaging process

---
+++ src/main/java/org/ouisncf/xspeedit/XSpeedItApplication.java
@@ -0,0 +1,50 @@
+package org.ouisncf.xspeedit;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * XSpeedIt application.
+ *
+ * Expected argument:
+ * 	the input articles to process as a chain of size values.
+ *
+ * @author Valentin Noel
+ *
+ */
+public class XSpeedItApplication {
+
+	/**
+	 * Display an usage help
+	 */
+	private static void displayUsage() {
+		System.out.println("Please indicate the input articles to process as a chain of size values.");
+		System.out.println("These size values must be included between 1 and 9.");
+		System.out.println("For example: 163841689525773");
+	}
+
+	/**
+	 * Application main method
+	 *
+	 * @param args
+	 * 	Input article sizes
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to XSpeedIt!\n");
+		if(args.length != 1) {
+			displayUsage();
+		}
+
+		final List<Article> articles = args[0].chars().boxed()
+				.map((c) -> Character.getNumericValue(c))
+				.map((size) -> new Article(size))
+				.collect(Collectors.toList());
+
+		final Robot robot = new Robot();
+		System.out.print(" - Linear process:\t");
+		robot.processPackaging(articles);
+		System.out.println(robot.getStringContent() + " => Number of boxes: " + robot.getNumberOfUsedBoxes());
+	}
+
+}
